bf68937f2df17738ed6a9b1b3d9b0d26feff71e0
Nonlinear Vibes <b.gabor_92@hotmail.com>
2025-10-29 00:19:35 +0100

    flexible parameter handling

--- models/SIR.m
+++ models/SIR.m
@@ -1,25 +1,18 @@
-function f = SIR(~, y)
-% SIR  Basic SIR compartmental epidemic model
-% Equations:
-%        S' = -beta * S * I
-%        I' =  beta * S * I - gamma * I
-%        R' =  gamma * I
+function f = SIR(~, y, p)
+% SIR: Basic SIR compartmental epidemic model.
+%   S' = -beta * S * I
+%   I' =  beta * S * I - gamma * I
+%   R' =  gamma * I
 
-% Parameters
-beta  = 0.30;    % infection rate
-gamma = 0.10;    % recovery rate
+if nargin < 3 || isempty(p), p = struct(); end
+if ~isfield(p,'beta'),  p.beta  = 0.30; end  % infection rate
+if ~isfield(p,'gamma'), p.gamma = 0.10; end  % recovery rate
 
-% Unpack states
-S = y(1);
-I = y(2);
-R = y(3);
+S = y(1); I = y(2); R = y(3);
+lambda = p.beta * S * I;
 
-lambda = beta * S * I;
-
-% System
-f  = [ -lambda;
-        lambda - gamma * I;
-        gamma * I];
-
+f = [ -lambda;
+       lambda - p.gamma * I;
+       p.gamma * I ];
 end
 
