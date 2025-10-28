eca3d040c7aa84f7bcead33b25feeedcca8361d0
Nonlinear Vibes <b.gabor_92@hotmail.com>
2025-10-29 00:27:13 +0100

    added function handler for Newton iteration

--- solvers/IE.m
+++ solvers/IE.m
@@ -12,8 +12,6 @@ m  = numel(y0);
 y  = zeros(N, m);
 y(1,:) = y0';
 
-maxIt = 5;
-
 for k = 1:N-1
     tk   = t(k);
     tkp1 = t(k+1);
@@ -23,7 +21,8 @@ for k = 1:N-1
     x  = yk + h*fk;     % Euler predictor
 
     % Solve G_BE(x) = x - yk - h f(t_{k+1}, x) = 0 via NewtonIt
-    x = NewtonIt(fun, tkp1, x, yk, fk, h, maxIt);
+    G = @(x) x - yk - h * fun(tkp1, x);
+    x = NewtonIt(G, x);
 
     % implicit Euler update is just the converged x
     % transpose to store as a row
