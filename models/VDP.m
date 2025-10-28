d9f9f55a1cc393bb1d9acb2179b86b2bb1c978d8
Nonlinear Vibes <b.gabor_92@hotmail.com>
2025-10-29 00:20:09 +0100

    flexible parameter handling

--- models/VDP.m
+++ models/VDP.m
@@ -1,15 +1,13 @@
-function f = VDP(~, y)
+function dy = VDP(~, y, params)
 % VDP  van der Pol oscillator with inline parameter mu.
-% State y = [y1; y2].  y1' = y2, y2' = mu*(1 - y1^2)*y2 - y1
+% State y = [y1; y2].  y1' = y2, y2' = mu*(1 - y1^2)*y2 - y1    
 
-% Nonlinear damping coeff.
-mu = 3;
-a  = 1; 
-b  = 1;
+    if nargin < 3 || isempty(params), params = struct(); end
+    if ~isfield(params, 'mu'), params.mu = 3.0; end
 
-% System
-f  = [ y(2);
-       mu*(a - y(1)^2)*y(2) - b*y(1) ];
+    x    = y(1);
+    xdot = y(2);
 
+    dy = [ xdot;
+           params.mu * (1 - x^2) * xdot - x ];
 end
-
